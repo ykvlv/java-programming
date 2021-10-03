1c0c6262786d279ac1fcc3af18f451decc6053e7
Grigory Yakovlev <ykvlv@niuitmo.ru>
2021-10-03 16:01:49 +0300

    multithread response added

--- lab7/src/common/StringDye.java
+++ lab7/src/common/StringDye.java
@@ -3,18 +3,21 @@ package common;
 public final class StringDye {
     private static final String START_OF_PATTERN = (char)27 + "[";
     private static final String END_OF_PATTERN = (char)27 + "[0m";
+    private static final String RED = "31m";
+    private static final String GREEN = "32m";
+    private static final String YELLOW = "33m";
 
     private StringDye() {}
 
     public static String red(String string) {
-        return START_OF_PATTERN + 31 + "m" + string + END_OF_PATTERN;
+        return START_OF_PATTERN + RED + string + END_OF_PATTERN;
     }
 
     public static String green(String string) {
-        return START_OF_PATTERN + 32 + "m" + string + END_OF_PATTERN;
+        return START_OF_PATTERN + GREEN + string + END_OF_PATTERN;
     }
 
     public static String yellow(String string) {
-        return START_OF_PATTERN + 33 + "m" + string + END_OF_PATTERN;
+        return START_OF_PATTERN + YELLOW + string + END_OF_PATTERN;
     }
 }
--- lab7/src/server/Server.java
+++ lab7/src/server/Server.java
@@ -1,12 +1,12 @@
 package server;
 
 import common.Request;
-import common.RequestType;
 import common.Response;
 import common.StringDye;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.SocketAddress;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -26,21 +26,25 @@ public class Server {
         this.commandExecutor = commandExecutor;
     }
 
+    public boolean isExit() throws IOException {
+        while (reader.ready()) {
+            String str = reader.readLine();
+            if ("exit".equals(str)) {
+                selector.close();
+                reader.close();
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void run() throws IOException, ClassNotFoundException {
         while (true) {
             if (selector.select(1000) == 0) {
                 //Чекер комманд на сервере
-                if (reader.ready()) {
-                    String str = reader.readLine();
-                    if ("exit".equals(str)) {
-                        selector.close();
-                        reader.close();
-                        return;
-                    } else {
-                        System.out.println(StringDye.yellow("Есть только exit"));
-                    }
+                if(isExit()) {
+                    return;
                 }
-                continue;
             }
 
             Set<SelectionKey> keys = selector.selectedKeys();
@@ -54,11 +58,12 @@ public class Server {
                         key.interestOps(SelectionKey.OP_WRITE);
 
                     } else if (key.isWritable()) {
-                        //Выполнить запрос закрепленный за ключом
+                        //Выполнить и отправить запрос закрепленный за ключом
                         Request request = (Request) key.attachment();
                         Response response = commandExecutor.execute(request);
                         System.out.println(response.getResponseType());
-                        serverIOHandler.writeTo(response, (DatagramChannel) key.channel(), request.getAddress());
+
+                        sendResponse(response, (DatagramChannel) key.channel(), request.getAddress());
                         key.interestOps(SelectionKey.OP_READ);
                     }
                 } else {
@@ -67,5 +72,15 @@ public class Server {
                 it.remove();
             }
         }
+    }
+
+    public void sendResponse(Response response, DatagramChannel channel, SocketAddress address) {
+        new Thread(() -> {
+            try {
+                serverIOHandler.writeTo(response, channel, address);
+            } catch (IOException e) {
+                System.out.println(StringDye.yellow("Не удалось отправить ответ"));
+            }
+        }).start();
     }
 }
